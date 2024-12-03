c834b71dc22fe6a275a7c065721045cb778bf9f0
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-12-03 14:40:36 +0200

    Importuotos reikiamos klases: Test, Assert, TextCounter. Apibrezta pagrindine testu klase TextCounterTest

--- app/src/test/java/com/example/textcounter/TextCounterTest.java
+++ app/src/test/java/com/example/textcounter/TextCounterTest.java
@@ -1,4 +1,10 @@
 package com.example.textcounter;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.example.textcounter.utils.TextCounter;
+
 public class TextCounterTest {
+
 }
